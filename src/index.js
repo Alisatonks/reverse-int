677f9626781e0257f096b981ebb9e8dedb556b1c
Volha Haiduk <Alisatonks2385@gmail.com>
2022-07-01 23:33:25 +0200

    docs: add decision

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    let absoluteNumber = Math.abs(n)
+  let i = absoluteNumber.toString().length
+    let result = ''
+    let numberStr = absoluteNumber.toString()
+  while (i > 0) {
+    result = `${result}${numberStr[i-1]}`
+    i = i - 1
+  }
+    
+    return result * 1
 }
+   
+
+
